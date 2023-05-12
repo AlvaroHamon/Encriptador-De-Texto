ccbd7ce17a076c79015791d12db5a7cb32a4964f
AlvaroHamon <alvaro.hamon@gmail.com>
2023-05-12 11:13:27 -0500

    agregando funcionalidad para revisar cadenas de texto en blanco

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // obtener elementos HTML
+const btn_encriptar = document.querySelector(".btn_encriptar");
+const btn_desencriptar = document.querySelector(".btn_desencriptar");
 const btn_limpiar = document.querySelector(".btn_limpiar");
 const btn_copiar = document.querySelector(".copiar")
 const text_area = document.querySelector(".text_area");
@@ -10,49 +12,45 @@ const mensaje = document.querySelector(".mensaje");
 // La letra "o" es convertida para "ober"
 // La letra "u" es convertida para "ufat"
 
-function btn_encriptar() {
-    const msnEncriptado = encriptar(text_area.value);
-    mensaje.value = msnEncriptado;
-}
-
-function btn_desencriptar() {
-    const msnDesencriptado = desencriptar(text_area.value);
-    mensaje.value = msnDesencriptado;
-    
-}
-
 function encriptar(strEncrip) {
 
-    let matriz = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-    strEncrip = strEncrip.toLowerCase();
-
-    for (let i = 0; i < matriz.length; i++) {
-        if (strEncrip.includes(matriz[i][0])) {
-            strEncrip = strEncrip.replaceAll(matriz[i][0], matriz[i][1]);
+    if (strEncrip.trim() === "") {
+        alert("Debe agregar el texto a Encriptar");
+    } else {
+        let matriz = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+        strEncrip = strEncrip.toLowerCase();
+        for (let i = 0; i < matriz.length; i++) {
+            if (strEncrip.includes(matriz[i][0])) {
+                strEncrip = strEncrip.replaceAll(matriz[i][0], matriz[i][1]);
+            }
         }
+        document.getElementById("muneco").style.display = "none";
+        document.getElementById("ningun_msn").style.display = "none";
+        document.getElementById("btn_copiar").style.display = "initial";
+        console.log(strEncrip);
+        mensaje.value = strEncrip;
+        return strEncrip;
     }
-
-    document.getElementById("muneco").style.display = "none";
-    document.getElementById("ningun_msn").style.display = "none";
-    document.getElementById("btn_copiar").style.display = "initial";
-    return strEncrip;
-
 }
 
 function desencriptar(strDesencrip) {
 
-    let matriz = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-    strDesencrip = strDesencrip.toLowerCase();
-
-    for (let i = 0; i < matriz.length; i++) {
-        if (strDesencrip.includes(matriz[i][1])) {
-            strDesencrip = strDesencrip.replaceAll(matriz[i][1], matriz[i][0]);
+    if (strDesencrip.trim() === "") {
+        alert("Debe agregar el texto a Desecriptar");
+    } else {
+        let matriz = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+        strDesencrip = strDesencrip.toLowerCase();
+        for (let i = 0; i < matriz.length; i++) {
+            if (strDesencrip.includes(matriz[i][1])) {
+                strDesencrip = strDesencrip.replaceAll(matriz[i][1], matriz[i][0]);
+            }
         }
+        document.getElementById("muneco").style.display = "none";
+        document.getElementById("ningun_msn").style.display = "none";
+        document.getElementById("btn_copiar").style.display = "initial";
+        mensaje.value=strDesencrip;
+        return strDesencrip;
     }
-    document.getElementById("muneco").style.display = "none";
-    document.getElementById("ningun_msn").style.display = "none";
-    document.getElementById("btn_copiar").style.display = "initial";
-    return strDesencrip;
 
 }
 
@@ -73,5 +71,7 @@ function copiar() {
 }
 
 limpiar();
+btn_encriptar.addEventListener('click', () => encriptar(text_area.value));
+btn_desencriptar.addEventListener('click', () => desencriptar(text_area.value));
 btn_limpiar.addEventListener('click', limpiar);
 btn_copiar.addEventListener("click", copiar);
